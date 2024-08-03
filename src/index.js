146bafd13cc2b6486875fec1b961ef5c597a43ab
Farhad Sadykov <f.sadykov@yahoo.com>
2024-08-03 22:32:30 +0300

    html for data output

--- src/index.js
+++ src/index.js
@@ -3,20 +3,21 @@ import './scss/main.scss';
 const url = 'https://api.nasa.gov/EPIC/api/natural/?api_key=';
 const apiKey = 'YyhXjir0NUcQWUKAMVTYSXxEsnKj8fuYa3KvVXoh';
 
-
 fetch(url + apiKey)
   .then(res => res.json())
   .then((data) => {
     console.log(data);
 
     const urlDate = data[0].date.slice(0, 10).split('-').join('/'); //один раз, без цикла
+    //здесь будет переменная с выводимой датой
 
     console.log(urlDate);
 
     const container = document.querySelector('.container');
-    container.innerHTML = `
-      <img src="https://epic.gsfc.nasa.gov/archive/natural/${urlDate}/png/${data[0].image}.png"/>
-    `;
+    // container.innerHTML = `
+    //   <img src="https://epic.gsfc.nasa.gov/archive/natural/${urlDate}/png/${data[0].image}.png"/>
+    // `;
 
   })
 
+{/* <img src="https://epic.gsfc.nasa.gov/archive/natural/${urlDate}/png/${data[0].image}.png"/> */}
