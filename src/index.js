03fe3a0d82111ad7d94f54de4f5f05e81314d0cf
Farhad Sadykov <f.sadykov@yahoo.com>
2024-08-05 02:20:42 +0300

    styles for data section

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const apiKey = 'YyhXjir0NUcQWUKAMVTYSXxEsnKj8fuYa3KvVXoh';
 fetch(url + apiKey)
   .then(res => res.json())
   .then((data) => {
-    console.log(data);
+    // console.log(data);
 
     const urlDate = data[0].date.slice(0, 10).split('-').join('/'); //один раз, без цикла
     //здесь будет переменная с выводимой датой
 
-    console.log(urlDate);
+    // console.log(urlDate);
 
     const container = document.querySelector('.container');
     // container.innerHTML = `
